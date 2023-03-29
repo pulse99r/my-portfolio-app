b1894ba9182a2153ef9f89876049313a8274ee05
Raymond <raymondjohnson@pursuit.org>
2023-03-29 09:37:01 -0400

    design considerations; JS and CSS modules

---
+++ assets/resume-cards.js
@@ -0,0 +1,6 @@
+const resumeCard = (section) => {
+  let resumeSection = resume.section
+}
+
+console.log(resume[0].objective)
+console.log(resume[0].employmentHistory[0])
--- assets/resume-data.js
+++ assets/resume-data.js
@@ -102,4 +102,4 @@ hobbies: [
  ]
   }
 ]
-console.log(resume)
+
