2ef77f7791cc16666f17d07b615fd7cd5a0b1bbc
Raymond <raymondjohnson@pursuit.org>
2023-03-29 07:57:56 -0400

    initial commit to setup basic files

---
+++ assets/resume-data.js
@@ -0,0 +1,105 @@
+const resume = [ 
+  {
+    objective: "Seeking an Entry Level or Apprenticeship Software Engineering position that offers training and development as well as an opportunity to leverage my skills and experience in support of the organization’s mission.",
+
+  employmentHistory: [ 
+    {
+      title: "Sr. Programmer Analyst | New York Life Insurance Co." ,
+      year: '1996 - 2009',
+      jobDescription: ['Provided technical support in various capacities including Desktop Support, Service Request Management','Assisted with Disaster Recovery Planning and Implementation, through the 9/11/2001 disaster and several others including two NYC Blackouts','Contributed to the implementation of Configuration Management tools and practices as one of the first cohort of people to be ITIL certified.']
+    },
+    {
+      title: "Helpdesk Support Analyst | May Department Store Int’l",
+      year: '1988 - 1996',
+      jobDescription: ["Provided technical support to community of over 500 staff members", "Managed deployment of Novell and OS/2 Lan Server Networks, enterprise conversion from typewriters to Personal Desktop computers", "Designed a system that allowed for a mobile network for capturing data during buying meetings that reduced processing time from 6 months to less than 30 days, saving the company millions of dollars and shortening the time it took to bring new products to market by more than nine months."
+    ]
+    },
+    {
+      title: "Administrative Assistant | CitiCorp",
+      year: "1986 - 1988",
+      jobDescription: ["Responsible for the acquisition, deployment and maintenance of new technology", "Accountable for coordinating or delivering training to Administrative Staff", "Documentation of best practices for integrating automation into workflow", "expense tracking", "travel/meeting scheduling"
+      ] 
+    }],
+  education: [
+    {school:"Pursuit Fellow",
+    startDate: "11/2022", 
+    endDate:"12/2023",
+    program: "Full Stack Web Development Training Program"
+    },
+    {school: "Udacity", 
+    startDate: "5/2022",
+    endDate: "9/2022",
+    program: "Online Front End Web Development NanoDegree Program"
+    },
+    {school: "Mercy College",
+    startDate: "1981", 
+    endDate: "1984",
+    program: "Certificate in Computer Science"
+    },
+    {school: "Anderson University",
+    startDate: "1978",
+    endDate: "1979",
+    program: "30 Credits in Music Performance",
+    },
+    {school: "LIU Brooklyn-Brooklyn" ,
+    year: "9/1977", 
+    endDate: "6/1978",
+    program: "30 Credits in Accounting"
+    },
+    {school:"Mount Vernon HS--Mount Vernon, NY",
+    startDate: "1972",
+    endDate: "1976"
+    },
+  ],
+  skills: [
+    {
+      frontEnd: "Front End: JavaScript, HTML5, CSS, JQery, Bootstrap Library, Git/GitHub",
+      database: "Database: Claris FileMaker Pro Database / Workflow App Development",
+      helpDeskSupport: "Help Desk Support: Windows, Mac OS, Microsoft Office / Office 365 / Word / Excel / PowerPoint / Access",
+      management: "Project Planning, Project Management",
+      softSkills: "Strong verbal and written communication, Teamwork, Hardware/Software Troubleshooting"
+    }
+  ],
+  volunteer: [
+  {
+    role: "Fresh Air Fund Camp Counselor",
+    description: "Volunteer each summer to teach Music or Arts & Crafts to Fresh Air Fund Campers."
+  },
+  {
+    role: "Landmark Worldwide", 
+    description: "Coached participants in various personal training and development programs. Utilize Excel and Google Sheets to organize and manage program information for three to six-month long programs."
+  },
+  {
+    role: "Toastmasters Club International",
+    description:"The Toastmasters Club provides a forum for members to practice and develop leadership skills through public speaking and taking on leadership positions within the club. I was active as the club's Sgt. At Arms and Secretary from 2007 through 2009. Sgt. At Arms is responsible for scheduling and organizing club meetings, elections for club officers, and ensuring all the needed materials for successful club meetings are available. Secretary is accountable for communications to club members, representing the club to the public and expanding club membership."
+  }
+  ],
+hobbies: [
+  {
+    role: "Guitarist, Music Entertainer and Music Teacher",
+    startDate: "Sept. 1998", 
+    endDate: "Present",
+    description: "Provide music lessons to adults and children."
+  },
+  {
+    role: "Recording Engineer:",
+    startDate: "Sept. 1998", 
+    endDate: "Present",
+    description: "Recorded to music collections on compact disks: one of jazz standards and one of original music."
+  },
+  {
+    role: "Roller Skating: Organized rollerskating club for roller-dance skatersGRecording Engineer: recorded to music collections on compact disks: one of jazz standards and one of original music.",
+    startDate: "Sept. 1988", 
+    endDate: "1994",
+    description: "Provide music lessons to adults and children."
+  },
+  {
+    role: "Knitting",
+    startDate: "Sept. 1988", 
+    endDate: "1994",
+    description: "Organized volunteer groups to make Christmas stockings and other items for children"
+  }
+ ]
+  }
+]
+console.log(resume)
