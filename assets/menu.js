e8e4cc2d1d4317deb0f29f63e272ed7521a8efba
Raymond <raymondjohnson@pursuit.org>
2023-04-03 10:21:17 -0400

    Project re-designed: Now displays movie posters.

--- assets/menu.js
+++ assets/menu.js
@@ -1,29 +1,31 @@
 let menuItems = ["Home", "About","Portfolio","Resume","Contact"];
 
 const createMenu = (menuItems) => {
-  const headArea = document.querySelector('.head-area') // 1
+  //const headArea = document.querySelector('.page-header') // 1
 
+  const container = document.querySelector('.container');
   const headerContainer = document.createElement('div')
   headerContainer.setAttribute('class', 'header-container')
   const pgHeader = document.createElement('h1')
-  pgHeader.innerText = "Oh, Yeah ... Portfolio!"
+  pgHeader.classList.add('page-header')
+  pgHeader.innerText = "Movie Poster"
   
   const menuBar = document.createElement('ul');
-  menuBar.setAttribute("class", 'menu no-list-items');
+  menuBar.setAttribute("class", 'menuItems no-list-items');
   for (const menuItem of menuItems) {
     let newMenuItem = document.createElement('li');
     if(menuItem === 'Resume'){
       newMenuItem = document.createElement('a');
-      newMenuItem.setAttribute("href","./my-resume.html")
+      newMenuItem.setAttribute("href","assets/my-resume.html")
       //assets/my-resume.html
       newMenuItem.innerHTML = 'Resume';
     }
-    newMenuItem.setAttribute("class", `menuItems no-list-items ${menuItem}`);
+    newMenuItem.setAttribute("class", `menuItem no-list-items ${menuItem}`);
     newMenuItem.innerText = menuItem;
     menuBar.append(newMenuItem);
     headerContainer.append(pgHeader);
     headerContainer.append(menuBar);
-    headArea.append(headerContainer)
+    container.append(headerContainer)
     
   }
 }
--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,128 @@
 // alert('Script loaded!')
-// add artist image (including mine)
-// add a scrollable window for music albums
-//add window for details about artist and selected album
-//add footer with SM links and link to full resume
+/* 
+1. user enters an movie name
+2. movie poster and deets are displayed on the page
+3. 
+
+*/
+const APIkey = '3a97947f';
 
 let cards = document.querySelectorAll('.card>p')
-console.log('cards = ',cards)
 for (let i = 0; i < cards.length; i++) {
   const card = cards[i];
   card.setAttribute('class','card-link');
 }
-console.log('cards = ',cards)  
+
+let container = document.querySelector('.container')
+
+const pageElements = ['input','movie-results']
+
+const createPageElements = () => {
+  
+  pageElements.forEach(pageElement => {
+    let newDivElement = document.createElement('div')
+    if(pageElement === 'input'){
+      let inputElement = createInputField();
+      newDivElement.append(inputElement)
+      let newButton = createNewButton()
+      newDivElement.append(newButton)
+    } else {
+      newDivElement.classList = pageElement
+    }
+    container.append(newDivElement)
+  });
+}
+
+const createInputField = () => {
+  let inputElement = document.createElement('input')
+  inputElement.type="text"
+  inputElement.id = 'get-movie'
+  inputElement.placeholder = 'Enter a movie title'
+  return inputElement;
+}
+
+const createNewButton = () => {
+  let newButton = document.createElement('BUTTON')
+  newButton.type = 'Submit'
+  newButton.innerText = "Get Movie";
+  newButton.id = 'button-click'
+  return newButton;
+}
+const createNewPageElement = (pageElement) => {
+  let newElement = document.createElement(pageElement);
+  
+  newElement.id = pageElement;
+  newElement.classList = pageElement;
+  return newElement;
+}
+createPageElements(pageElements)
+
+let getMovieClick = document.querySelector('#button-click')
+const movieInfo = getMovieClick.addEventListener('click', (event)=>{
+  event.preventDefault;
+  let movieTitle = document.querySelector('#get-movie').value;
+  console.log('Click Event Happened!', "Title: ", movieTitle)
+    fetch(`http://www.omdbapi.com/?t=${movieTitle}&plot=full&apikey=${APIkey}`)
+      .then(response => response.json())
+      .then(response => displayMovieDetails(response))
+      .catch(err => console.error(err));
+      console.log('movieTitle: ==> ',movieTitle)
+})
+
+const createNewElement = (element, attrib, val) => {
+ let newElement = document.createElement(element)
+ newElement.setAttribute(attrib,val);
+ return newElement;
+}
+
+const displayMovieDetails = (movie) => {
+  console.log(movie)
+  let movieResultsDiv = document.querySelector('.movie-results')
+
+  //let myMovieObj = (Title,Released,Runtime,Genre,Actors,Plot,Awards,Poster,imdbID) = movie; 
+  let moviePosterCard = createNewElement('ul', 'class', 'movie-poster-card')
+  let posterLiElement = createNewElement('li','class','list-item poster-container');
+  let posterImgElement = createNewElement('img','src',movie.Poster );
+  posterLiElement.append(posterImgElement)
+  moviePosterCard.append(posterLiElement);
+  
+  let titleLi = createNewElement('li','class', 'list-item title')
+  titleLi.innerText = movie.Title;
+  moviePosterCard.append(titleLi);
+  
+  let actorsLi = createNewElement('li','class', 'list-item actors')
+  actorsLi.innerText = movie.Actors;
+  moviePosterCard.append(actorsLi);
+  
+  let releaseLi = createNewElement('li', 'class', 'list-item release');
+  releaseLi.innerHTML = `<strong>Released:</strong> ${movie.Released}`;
+  
+  let genreLi = createNewElement('li', 'class', 'list-item genre');
+  genreLi.innerHTML = `<strong>Genre:</strong> ${movie.Genre}`;
+  
+  let genreReleasedRuntimeLi = createNewElement('li', 'class', 'list-item runtime');
+  genreReleasedRuntimeLi.innerHTML = `<strong>Genre:</strong> ${movie.Genre} • <strong>Released:</strong> ${movie.Released} • <strong>Runtime:</strong> ${movie.Runtime}`;
+  moviePosterCard.append(genreReleasedRuntimeLi);
+  
+  let plotLi = createNewElement('li', 'class', 'list-item plot');
+  plotLi.innerHTML = `<strong>Plot:</strong> ${movie.Plot}`;
+  moviePosterCard.append(plotLi);
+  
+  let awardsLi = createNewElement('li', 'class', 'list-item awards');
+  awardsLi.innerHTML= `<strong>Awards:</strong> ${movie.Awards}`;
+  moviePosterCard.append(awardsLi);
+  
+  let imdbIdLi = createNewElement('li', 'class', 'list-item imdb');
+  imdbIdLi.innerHTML = `<strong>imdbId:</strong> ${movie.imdbID}`;
+  moviePosterCard.append(imdbIdLi);
+
+  movieResultsDiv.append(moviePosterCard)
+  
+ 
+  // newDivElement.append(newElement);
+  // let newImgTag = createNewPageElement('img')
+  // newDivElement.append(newImgTag);
+  // let newUlTag = createNewPageElement('ul')
+  // newDivElement.append(newUlTag);
+  // movieResultsDiv.append(newImgElement)
+}
